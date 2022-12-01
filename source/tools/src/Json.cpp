a29c328fe4bc2f5a5b2d9cf1ec8b0c51c2b91f3e
Simon Zimmermann <zimmermannsimon92@gmail.com>
2022-12-01 17:21:49 +0100

    Fixed problem with demangle in windows

--- source/tools/src/Json.cpp
+++ source/tools/src/Json.cpp
@@ -1,17 +1,20 @@
 #include <lenny/tools/Json.h>
 
 #if WIN32
-const char* demangle(const char* name) {
-    return name;
+std::string demangle(const std::string& name) {
+    std::stringstream ss(name);
+    std::istream_iterator<std::string> begin(ss), end;
+    const auto list = std::vector<std::string>(begin, end);
+    return list.back();
 }
 #else
 #include <cxxabi.h>
 
-const char* demangle(const char* name) {
+std::string demangle(const std::string& name) {
     char buf[1024];
     size_t size = 1024;
     int status;
-    char* res = abi::__cxa_demangle(name, buf, &size, &status);
-    return res;
+    char* res = abi::__cxa_demangle(name.c_str(), buf, &size, &status);
+    return std::string(res);
 }
 #endif
