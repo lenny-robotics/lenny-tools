e02cd3a8ab39d0b576445951448a4e63923c65e7
Simon Zimmermann <40738671+SimonZ92@users.noreply.github.com>
2022-12-20 21:28:45 +0200

    Softification weights (#3)
    * New strategy to use finite difference class (not as base class, but as member)
    
    * Removed isCheckBeingApplied bool from FiniteDifference

--- source/tools/include/lenny/tools/FiniteDifference.h
+++ source/tools/include/lenny/tools/FiniteDifference.h
@@ -15,57 +15,51 @@ public:
     typedef std::function<void(Eigen::SparseMatrixD& sMat, const Eigen::VectorXd& x)> F_SMat;
     typedef std::function<void(Eigen::MatrixXd& dMat, const Eigen::VectorXd& x)> F_DMat;
     typedef std::function<void(Eigen::TensorD& ten, const Eigen::VectorXd& x)> F_Ten;
+    typedef std::function<void(const Eigen::VectorXd& x)> F_PreEval;
 
     //--- Constructor
     FiniteDifference(const std::string& description) : description(description) {}
-    virtual ~FiniteDifference() = default;
+    ~FiniteDifference() = default;
 
     //--- Estimators
-    virtual void estimateVector(Eigen::VectorXd& vec, const Eigen::VectorXd& x, const F_Val& eval) const;
-    virtual void estimateMatrix(Eigen::TripletDList& tripL, const Eigen::VectorXd& x, const F_Vec& eval, const uint& firstDim, bool fullMatrix) const;
-    virtual void estimateMatrix(Eigen::SparseMatrixD& sMat, const Eigen::VectorXd& x, const F_Vec& eval, const uint& firstDim, bool fullMatrix) const;
-    virtual void estimateMatrix(Eigen::MatrixXd& dMat, const Eigen::VectorXd& x, const F_Vec& eval, const uint& firstDim, bool fullMatrix) const;
-    virtual void estimateTensor(Eigen::TensorD& ten, const Eigen::VectorXd& x, const F_TripL& eval, const uint& firstDim, const uint& secondDim) const;
-    virtual void estimateTensor(Eigen::TensorD& ten, const Eigen::VectorXd& x, const F_SMat& eval, const uint& firstDim, const uint& secondDim) const;
-    virtual void estimateTensor(Eigen::TensorD& ten, const Eigen::VectorXd& x, const F_DMat& eval, const uint& firstDim, const uint& secondDim) const;
+    void estimateVector(Eigen::VectorXd& vec, const Eigen::VectorXd& x, const F_Val& eval) const;
+    void estimateMatrix(Eigen::TripletDList& tripL, const Eigen::VectorXd& x, const F_Vec& eval, const uint& firstDim, bool fullMatrix) const;
+    void estimateMatrix(Eigen::SparseMatrixD& sMat, const Eigen::VectorXd& x, const F_Vec& eval, const uint& firstDim, bool fullMatrix) const;
+    void estimateMatrix(Eigen::MatrixXd& dMat, const Eigen::VectorXd& x, const F_Vec& eval, const uint& firstDim, bool fullMatrix) const;
+    void estimateTensor(Eigen::TensorD& ten, const Eigen::VectorXd& x, const F_TripL& eval, const uint& firstDim, const uint& secondDim) const;
+    void estimateTensor(Eigen::TensorD& ten, const Eigen::VectorXd& x, const F_SMat& eval, const uint& firstDim, const uint& secondDim) const;
+    void estimateTensor(Eigen::TensorD& ten, const Eigen::VectorXd& x, const F_DMat& eval, const uint& firstDim, const uint& secondDim) const;
 
     //--- Testers
-    virtual bool testVector(const F_Val& eval, const F_Vec& ana, const Eigen::VectorXd& x, const std::string& name) const;
-    virtual bool testMatrix(const F_Vec& eval, const F_TripL& ana, const Eigen::VectorXd& x, const std::string& name, const uint& firstDim,
-                            const bool& fullMatrix) const;
-    virtual bool testMatrix(const F_Vec& eval, const F_SMat& ana, const Eigen::VectorXd& x, const std::string& name, const uint& firstDim,
-                            const bool& fullMatrix) const;
-    virtual bool testMatrix(const F_Vec& eval, const F_DMat& ana, const Eigen::VectorXd& x, const std::string& name, const uint& firstDim,
-                            const bool& fullMatrix) const;
-    virtual bool testTensor(const F_TripL& eval, const F_Ten& ana, const Eigen::VectorXd& x, const std::string& name, const uint& firstDim,
-                            const uint& secondDim) const;
-    virtual bool testTensor(const F_SMat& eval, const F_Ten& ana, const Eigen::VectorXd& x, const std::string& name, const uint& firstDim,
-                            const uint& secondDim) const;
-    virtual bool testTensor(const F_DMat& eval, const F_Ten& ana, const Eigen::VectorXd& x, const std::string& name, const uint& firstDim,
-                            const uint& secondDim) const;
-
-    //--- Helpers
-    virtual void setFDCheckIsBeingApplied(bool isBeingApplied) const;
-    virtual void preFDEvaluation(const Eigen::VectorXd& x) const {}
+    bool testVector(const F_Val& eval, const F_Vec& ana, const Eigen::VectorXd& x, const std::string& name) const;
+    bool testMatrix(const F_Vec& eval, const F_TripL& ana, const Eigen::VectorXd& x, const std::string& name, const uint& firstDim,
+                    const bool& fullMatrix) const;
+    bool testMatrix(const F_Vec& eval, const F_SMat& ana, const Eigen::VectorXd& x, const std::string& name, const uint& firstDim,
+                    const bool& fullMatrix) const;
+    bool testMatrix(const F_Vec& eval, const F_DMat& ana, const Eigen::VectorXd& x, const std::string& name, const uint& firstDim,
+                    const bool& fullMatrix) const;
+    bool testTensor(const F_TripL& eval, const F_Ten& ana, const Eigen::VectorXd& x, const std::string& name, const uint& firstDim,
+                    const uint& secondDim) const;
+    bool testTensor(const F_SMat& eval, const F_Ten& ana, const Eigen::VectorXd& x, const std::string& name, const uint& firstDim, const uint& secondDim) const;
+    bool testTensor(const F_DMat& eval, const F_Ten& ana, const Eigen::VectorXd& x, const std::string& name, const uint& firstDim, const uint& secondDim) const;
 
     //--- Printers
-    bool performFDCheck(const Eigen::VectorXd& estimate, const Eigen::VectorXd& analytic, const std::string& name) const;
-    bool performFDCheck(const Eigen::MatrixXd& estimate, const Eigen::MatrixXd& analytic, const std::string& name) const;
-    bool performFDCheck(const Eigen::TensorD& estimate, const Eigen::TensorD& analytic, const std::string& name) const;
+    bool performCheck(const Eigen::VectorXd& estimate, const Eigen::VectorXd& analytic, const std::string& name) const;
+    bool performCheck(const Eigen::MatrixXd& estimate, const Eigen::MatrixXd& analytic, const std::string& name) const;
+    bool performCheck(const Eigen::TensorD& estimate, const Eigen::TensorD& analytic, const std::string& name) const;
 
 public:
+    //--- Members
     std::string description;  //Set by constructor
-    bool printTestMatricesToFile = false;
+    F_PreEval f_PreEval = nullptr;
+    bool printMatricesToFile = false;
+    bool printCheck = true;
 
-    //--- FD tolerances
-    double deltaFD = 1e-6;
-    double relTolFD = 1e-4;
-    double absTolFD = 1e-6;
-    double epsFD = 1e-10;
-    bool printFDCheck = true;
-
-protected:
-    mutable bool fdCheckIsBeingApplied = false;
+    //--- Tolerances
+    double delta = 1e-6;
+    double relTol = 1e-4;
+    double absTol = 1e-6;
+    double eps = 1e-10;
 };
 
 }  // namespace lenny::tools
